0c7f3deb352bcf212f7f72d1b95a6447347afac4
luizhsalazar <luizhsalazar@gmail.com>
2015-09-16 15:26:11 -0300

    Adding capistrano gems

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :repo_url, 'git@github.com:luizhsalazar/SLR.git'
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-set :deploy_to, '/home/deploy/SLR'
+set :deploy_to, '/home/luiz/SLR'
 
 # Default value for :scm is :git
 # set :scm, :git
